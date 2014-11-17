1f1cbf13ca8db9ae59705771c39e7e8b3bbe1ce0
mbenba01 <mbenba01@mail.bbk.ac.uk>
2014-11-17 15:34:18 +0000

    Added file by Mustapha Benbaziz

---
+++ src/Test/myClass.java
@@ -0,0 +1,10 @@
+
+public class myClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Test: try to use my repository as contributor");
+		System.out.print("1-Clone repo, 2-create java file without affecting other files, 3-push new file to repo.");
+	}
+
+}
